7350d799c75a73f6326c9e3fcb531efacfd793a4
brendapaz1 <brendapaz1771@gmail.com>
2019-04-26 17:27:06 -0700

    holis

---
+++ src/test/java/edu/ucsd/cs/StepsForDayTest.java
@@ -0,0 +1,71 @@
+package edu.ucsd.cs;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StepsForDayTest {
+    StepsForDay sfd;
+
+    @BeforeEach
+    void setUp() {
+        sfd = new StepsForDay(5000);
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void inputSteps() {
+        sfd.inputSteps(2000);
+        int steps = sfd.get();
+        assertEquals(steps,2000);
+    }
+
+    @Test
+    void addSteps() {
+        sfd.inputSteps(2000);
+        sfd.addSteps(1000);
+        int steps = sfd.get();
+        assertEquals(steps,1000+2000);
+    }
+
+    @Test
+    void clearSteps() {
+        sfd.clearSteps();
+        int steps = sfd.get();
+        assertEquals(steps, 0);
+
+    }
+
+    @Test
+    void get() {
+        int steps = sfd.get();
+        assertEquals(steps, 0);
+
+        sfd.inputSteps(2000);
+        steps = sfd.get();
+        assertEquals(steps, 2000);
+    }
+
+    @Test
+    void metGoal() {
+        boolean result = sfd.metGoal();
+        assertFalse(result);
+
+        sfd.inputSteps(2000);
+        result = sfd.metGoal();
+        assertFalse(result);
+
+        sfd.inputSteps(5000);
+        result = sfd.metGoal();
+        assertTrue(result);
+
+        sfd.inputSteps(5000+1);
+        result = sfd.metGoal();
+        assertTrue(result);
+    }
+}
